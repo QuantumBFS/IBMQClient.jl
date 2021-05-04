f324ed7393e6958dfa9ac21d4c3fad661b8201b3
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-04 08:47:28 -0400

    test cancel

--- test/adapters.jl
+++ test/adapters.jl
@@ -112,6 +112,7 @@ const ALL = "a"
 const NONE = "n"
 const DONE = "d"
 const SIGINT = "\x03"
+const QUIT = "q"
 
 menu = IBMQClient.DeviceMenu(devices)
 print(
@@ -133,6 +134,29 @@ print(
     LF,
     CR,
 )
-
 choice = request("choose a device:", menu)
 @test choice == 4
+readavailable(stdin.buffer)
+
+print(
+    stdin.buffer,
+    DOWN,
+    DOWN,
+    DOWN,
+    QUIT,
+)
+choice = request("choose a device:", menu)
+@test choice == -1
+readavailable(stdin.buffer)
+
+@test_throws InterruptException begin
+    print(
+        stdin.buffer,
+        DOWN,
+        DOWN,
+        DOWN,
+        SIGINT,
+    )
+    request("choose a device:", menu)
+    readavailable(stdin.buffer)
+end
